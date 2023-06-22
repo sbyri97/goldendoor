30eef94520a462ed8d02e686706c93040c21168c
Sai Byri <85528106+sbyri97@users.noreply.github.com>
2023-06-21 20:40:25 -0700

    added favicon

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -46,12 +46,12 @@ const Feed = (props) => {
     }
 
     return (
-        <div className="image-grid">
-            {post}
-        </div>
-        // <React.Fragment>
-        //     {post}
-        // </React.Fragment>
+      <div className="image-grid">
+        {post}
+      </div>
+      // <React.Fragment>
+      //     {post}
+      // </React.Fragment>
     );
 }
 
