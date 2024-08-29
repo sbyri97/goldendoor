be5f697fb7d7094a37f2bff9f74438b912187b21
Sai Byri <85528106+sbyri97@users.noreply.github.com>
2024-08-29 12:00:34 -0700

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,7 +17,7 @@ const About = () => {
           <h1>About Us</h1>
           <h5>Barber</h5>
           <p>
-            Hey, I'm Bilal, a passionate barber from Jordan. With 17 years of
+            Hey, I'm Bilal, a passionate barber from Jordan. With 18 years of
             experience, I've been shaping hairstyles in San Francisco for the
             past decade. I love what I do and take pride in creating clean,
             stylish cuts that boost confidence. My goal is to bring out the best
