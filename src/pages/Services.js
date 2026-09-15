70866c681ac64775781387a2c4353167782c8c1b
Sai Byri <85528106+sbyri97@users.noreply.github.com>
2026-09-15 10:43:21 -0700

    Update prices

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -30,13 +30,13 @@ const Services = () => {
         <div className="box">
           <img src={haircut} alt="" />
           <h3>Haircut</h3>
-          <h4>$50</h4>
+          <h4>$55</h4>
         </div>
 
         <div className="box">
           <img src={hairAndBeard} alt="" />
           <h3>Haircut & Beard</h3>
-          <h4>$70</h4>
+          <h4>$75</h4>
         </div>
 
         <div className="box">
